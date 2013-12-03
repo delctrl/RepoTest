fc5442508056fac8949c78badd5a246d9ddf4d9c
RODRIGO PEREIRA ASSUNCAO <rodrigo.passuncao@CAS424938M439-2.local>
2013-12-03 17:28:14 -0200

    Initial Commit

---
+++ AttGraphics/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AttGraphics
+//
+//  Created by RODRIGO PEREIRA ASSUNCAO on 03/12/13.
+//  Copyright (c) 2013 RODRIGO PEREIRA ASSUNCAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AttGraphicsTests/AttGraphicsTests.m
@@ -0,0 +1,34 @@
+//
+//  AttGraphicsTests.m
+//  AttGraphicsTests
+//
+//  Created by RODRIGO PEREIRA ASSUNCAO on 03/12/13.
+//  Copyright (c) 2013 RODRIGO PEREIRA ASSUNCAO. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AttGraphicsTests : XCTestCase
+
+@end
+
+@implementation AttGraphicsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
